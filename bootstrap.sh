4eada6617307b370c09af0331a851c00e903e770
RcPilot33 <kc8sby@gmail.com>
2026-07-19 12:53:51 -0400

    Add one-line bootstrap wrapper

---
+++ bootstrap.sh
@@ -0,0 +1,46 @@
+#!/bin/ash
+set -e
+
+# One-line installer entry point.
+# Downloads the full bootstrap folder to /tmp/bootstrap,
+# then runs the internal Jacob-style bootstrap script.
+
+REPO="Rcpilot33/k2-1155-Jacob-Fork"
+BRANCH="k2-1155-compat"
+BASE_URL="https://raw.githubusercontent.com/$REPO/$BRANCH"
+
+BOOTSTRAP_DIR="/tmp/bootstrap"
+
+rm -rf "$BOOTSTRAP_DIR"
+mkdir -p "$BOOTSTRAP_DIR/entware" "$BOOTSTRAP_DIR/better-root"
+
+download() {
+    SRC="$1"
+    DST="$2"
+
+    echo "Downloading $DST"
+    python3 - "$SRC" "$DST" <<'PY'
+import sys
+import urllib.request
+
+src = sys.argv[1]
+dst = sys.argv[2]
+
+urllib.request.urlretrieve(src, dst)
+PY
+}
+
+download "$BASE_URL/bootstrap/bootstrap.sh" "$BOOTSTRAP_DIR/bootstrap.sh"
+download "$BASE_URL/bootstrap/entware/install.sh" "$BOOTSTRAP_DIR/entware/install.sh"
+download "$BASE_URL/bootstrap/entware/wget-ssl.py" "$BOOTSTRAP_DIR/entware/wget-ssl.py"
+download "$BASE_URL/bootstrap/entware/unslung.init" "$BOOTSTRAP_DIR/entware/unslung.init"
+download "$BASE_URL/bootstrap/entware/unslung.init.d" "$BOOTSTRAP_DIR/entware/unslung.init.d"
+download "$BASE_URL/bootstrap/better-root/install.sh" "$BOOTSTRAP_DIR/better-root/install.sh"
+download "$BASE_URL/bootstrap/better-root/README.md" "$BOOTSTRAP_DIR/better-root/README.md"
+
+chmod +x "$BOOTSTRAP_DIR/bootstrap.sh"
+chmod +x "$BOOTSTRAP_DIR/entware/install.sh"
+chmod +x "$BOOTSTRAP_DIR/better-root/install.sh"
+
+cd "$BOOTSTRAP_DIR"
+sh ./bootstrap.sh "$@"
--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -9,8 +9,10 @@ sh ./entware/install.sh
 
 mkdir -p /mnt/UDISK/root
 cd /mnt/UDISK/root
-/opt/bin/git clone https://github.com/Jacob10383/k2-improvements.git
+
+/opt/bin/git clone -b k2-1155-compat https://github.com/Rcpilot33/k2-1155-Jacob-Fork.git k2-improvements
 
 cd $CURDIR
+
 # Run install script from better-root
 sh ./better-root/install.sh
