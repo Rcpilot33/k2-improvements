4b6aa14eb8569591109f51c692a549ba654884ef
RcPilot33 <kc8sby@gmail.com>
2026-08-14 18:34:28 -0400

    fix: restart after idempotent abort-homing install

--- features/abort_homing/install.sh
+++ features/abort_homing/install.sh
@@ -5,7 +5,7 @@ SCRIPT_DIR="$(readlink -f $(dirname $0))"
 python ${SCRIPT_DIR}/patch_webhooks.py /mnt/UDISK/root/klipper/klippy/webhooks.py
 EXIT_CODE=$?
 
-if [ $EXIT_CODE -eq 0 ]; then
+if [ $EXIT_CODE -eq 0 ] || [ $EXIT_CODE -eq 2 ]; then
     rm -f /mnt/UDISK/root/klipper/klippy/webhooks.pyc
     sh ${SCRIPT_DIR}/../../scripts/firmware_restart.sh
 elif [ $EXIT_CODE -eq 1 ]; then
